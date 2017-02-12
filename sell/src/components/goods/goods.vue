e1f81fb5451922522004484275ae1adb5407feb9
apple <18576469764@163.com>
2017-02-12 19:47:49 +0800

    scroll

--- sell/src/components/goods/goods.vue
+++ sell/src/components/goods/goods.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper">
+    <div class="menu-wrapper" ref="menuWrapper">
       <ul>
-        <li v-for="item in goods" class="menu-item">
+        <li v-for="(item, index) in goods" class="menu-item" :class="{'current':currentIndex === index}"
+            @click="selectMenu(index, $event)">
           <span class="text border-1px" >
             <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
           </span>
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper">
+    <div class="foods-wrapper" ref="foodWrapper">
       <ul>
-        <li v-for="item in goods" class="foot-list">
+        <li v-for="item in goods" class="foot-list" ref="foodList">
           <h1 class="title">{{item.name}}</h1>
           <ul>
             <li v-for="food in item.foods" class="food-item border-1px">
@@ -39,6 +40,8 @@
 </template>
 
 <script type="text/ecmascript-6">
+  import BScroll from 'better-scroll';
+
   const ERR_OK = 0;
   export default {
       props: {
@@ -48,19 +51,69 @@
       },
       data() {
           return {
-              goods: []
+              goods: [],
+              listHeight: [],
+              scrollY: 0
           };
       },
-    created() {
-          this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
-
-          this.$http.get('api/goods').then((response) => {
-              response = response.body;
-              if (response.erron === ERR_OK) {
-                this.goods = response.data;
+      computed: {
+        currentIndex() {
+            for (let i = 0; i < this.listHeight.length; i++) {
+              let height1 = this.listHeight[i];
+              let height2 = this.listHeight[i + 1];
+              if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+                return i;
               }
-          });
-    }
+            }
+            return 0;
+        }
+      },
+      created() {
+            this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+
+            this.$http.get('api/goods').then((response) => {
+                response = response.body;
+                if (response.erron === ERR_OK) {
+                  this.goods = response.data;
+                  this.$nextTick(() => {
+                    this._initScroll();
+                    this._calculateHeight();
+                  });
+                }
+            });
+      },
+      methods: {
+        selectMenu(index, event) {
+          if (!event._constructed) {
+              return;
+          }
+          let foodList = this.$refs.foodList;
+          let el = foodList[index];
+          this.foodsScroll.scrollToElement(el, 300);
+        },
+        _initScroll() {
+            this.meunScroll = new BScroll(this.$refs.menuWrapper, {
+              click: true
+            });
+            this.foodsScroll = new BScroll(this.$refs.foodWrapper, {
+              probeType: 3
+            });
+            this.foodsScroll.on('scroll', (pos) => {
+              this.scrollY = Math.abs(Math.round(pos.y));
+            });
+        },
+        _calculateHeight() {
+            let foodList = this.$refs.foodList;
+            let height = 0;
+            this.listHeight.push(height);
+            for (let i = 0; i < foodList.length; i++) {
+                let item = foodList[i];
+                height += item.clientHeight;
+                this.listHeight.push(height);
+            }
+        }
+
+      }
   };
 </script>
 
@@ -83,6 +136,14 @@
         width: 56px
         padding: 0 12px
         line-height: 14px
+        &.current
+          position: relative
+          z-index: 10
+          margin-top: -1px
+          background: #fff
+          font-weight: 700
+          .text
+            border-none()
         .icon
           display: inline-block
           vertical-align: top
@@ -142,8 +203,9 @@
             color: rgb(147, 153, 159)
           .desc
             margin-bottom: 8px
+            line-height: 12px
           .extra
-            &.count
+            .count
               margin-right: 12px
           .price
             font-weight: 700
